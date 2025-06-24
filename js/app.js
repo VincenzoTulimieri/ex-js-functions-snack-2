2c0b98368615178e1029ab921fa4702239e760af
VincenzoTulimieri <vincenzo22tulimieri@gmail.com>
2025-06-24 17:28:29 +0200

    feat: snack 4 completato

--- js/app.js
+++ js/app.js
@@ -1,41 +1,58 @@
 // snack 1
 
 // funzione dichiarativa 
-function somma(num1, num2){
+function somma(num1, num2) {
     return num1 + num2
 }
 
-console.log(somma(2,3))
+console.log(somma(2, 3))
 
 // funzione anonima assegnata ad una variabile
-const somma2 = function (num1,num2) {
+const somma2 = function (num1, num2) {
     return num1 + num2
 }
 
-console.log(somma2(2,5))
+console.log(somma2(2, 5))
 
 // arrow function 
 
-const somma3 =(num1,num2)=>{
+const somma3 = (num1, num2) => {
     return num1 + num2
 }
 
-console.log(somma3(4,5))
+console.log(somma3(4, 5))
 
+console.log('--------')
 // Snack 2
 
-const quadrato = (num)=> num * num 
+const quadrato = (num) => num * num
 
 console.log(quadrato(5))
 
+console.log('--------')
 //  Snack 3
 
-const sum = (num1,num2) => num1 + num2
-const moltiplicazione = (num1,num2) => num1 * num2
+const sum = (num1, num2) => num1 + num2
+const moltiplicazione = (num1, num2) => num1 * num2
 
-const eseguiOperazione = (num1,num2, operazione) => {
-    return operazione(num1,num2)
+const eseguiOperazione = (num1, num2, operazione) => {
+    return operazione(num1, num2)
 }
 
-console.log(eseguiOperazione(4,3, sum))
-console.log(eseguiOperazione(4,3, moltiplicazione))
+console.log(eseguiOperazione(4, 3, sum))
+console.log(eseguiOperazione(4, 3, moltiplicazione))
+
+console.log('--------')
+// snack 4 
+
+function creaTimer(time) {
+    return function () {
+        console.log(`avvio il timer ${time} ms`)
+        setTimeout(() => {
+            console.log('tempo scaduto')
+        }, time)
+    }
+}
+
+const timer = creaTimer(5000)
+timer()
--- readme.md
+++ readme.md
@@ -19,3 +19,9 @@
 ### Crea una funzione eseguiOperazione
 
 - Definisci una funzione eseguiOperazione che accetta tre parametri: due numeri e una funzione operatore (callback). La funzione deve eseguire l'operazione fornita sui due numeri.
+
+
+## Snack 4
+### Crea un generatore di funzioni creaTimer
+
+- Scrivi una funzione creaTimer che accetta un tempo (in ms) e restituisce una nuova funzione che avvia un setTimeout per stampare "Tempo scaduto!".
