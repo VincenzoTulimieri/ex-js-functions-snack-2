4eccb762580fc70115b6797e9872ce736ac55550
VincenzoTulimieri <vincenzo22tulimieri@gmail.com>
2025-06-24 16:20:04 +0200

    feat: snack 1 completato

---
+++ js/app.js
@@ -0,0 +1,28 @@
+// snack 1
+
+// funzione dichiarativa 
+function somma(num1, num2){
+    return num1 + num2
+}
+
+console.log(somma(2,3))
+
+// funzione anonima assegnata ad una variabile
+const somma2 = function (num1,num2) {
+    return num1 + num2
+}
+
+console.log(somma2(2,5))
+
+// arrow function 
+
+const somma3 =(num1,num2)=>{
+    return num1 + num2
+}
+
+console.log(somma3(4,5))
+
+
+
+
+
